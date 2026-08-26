72700b87a1457f00f6a9a3844c38e123299f472d
Mijael <mijael.lino@tecsup.edu.pe>
2026-08-26 15:00:31 -0500

    agregamos buscarProducto y eliminarProducto en CarritoPOO

--- Semana2/app/src/main/java/com/Lino/lab02carritokotlin/Carrito.kt
+++ Semana2/app/src/main/java/com/Lino/lab02carritokotlin/Carrito.kt
@@ -34,6 +34,7 @@ class CarritoPOO {
         total > 3000 -> total * 0.05
         else -> 0.0
     }
+
     fun mostrarDetalle() {
         println("---------------- DETALLE DEL CARRITO ----------------")
         productos.forEachIndexed { i, p -> println("${i + 1}. ${p.mostrarInfo()}") }
@@ -46,6 +47,14 @@ class CarritoPOO {
         productos.add(producto)
         println("Producto agregado: ${producto.nombre}")
     }
+
+    fun buscarProducto(nombre: String): Producto? {
+        return productos.find { it.nombre == nombre }
+    }
+
+    fun eliminarProducto(nombre: String) {
+        productos.removeIf { it.nombre == nombre }
+    }
 }
 
 fun main() {
@@ -54,10 +63,46 @@ fun main() {
     println("=========================================")
 
     val nombreCliente = "Mijael Lino"
-
+    val carrito = CarritoPOO()
 
     println("Cliente: $nombreCliente")
     println()
 
+    carrito.agregar(ProductoElectronico("Laptop HP", 2500.0, 1, garantiaMeses = 12))
+    carrito.agregar(ProductoAccesorio("Mouse Logitech", 45.5, 2))
+    carrito.agregar(ProductoElectronico("Audifonos Sony", 120.0, 1, garantiaMeses = 6))
+    carrito.agregar(ProductoAccesorio("USB Kingston 64GB", 25.0, 3))
+
+    println()
+    carrito.mostrarDetalle()
+    println("Cantidad de productos : ${carrito.cantidadProductos()}")
+
+    val subtotal = carrito.calcularSubtotal()
+    val igv = carrito.calcularIGV(subtotal)
+    val total = carrito.calcularTotal(subtotal, igv)
+
+    println(String.format("%-22s: S/ %8.2f", "Subtotal", subtotal))
+    println(String.format("%-22s: S/ %8.2f", "IGV (18%)", igv))
+    println(String.format("%-22s: S/ %8.2f", "TOTAL A PAGAR", total))
+
+    println()
+    val masCaro = carrito.productoMasCaro()
+    if (masCaro != null) {
+        println("Producto mas caro: ${masCaro.nombre} " + String.format("(S/%.2f)", masCaro.precio))
+    }
+
+    val descuento = carrito.calcularDescuento(total)
+    val totalConDescuento = total - descuento
+
+    if (total > 5000) {
+        println("Descuento aplicado: 10% por compra mayor a S/ 5000")
+    } else if (total > 3000) {
+        println("Descuento aplicado: 5% por compra mayor a S/ 3000")
+    } else {
+        println("Descuento aplicado: Sin descuento")
+    }
+
+    println(String.format("%-22s: S/ %8.2f", "TOTAL CON DESCUENTO", totalConDescuento))
+    println()
     println("Gracias por su compra, $nombreCliente!")
 }
