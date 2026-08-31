1d3e4acdb823c8bf77f81f2964d77f7c6004fe71
Mijael <mijael.lino@tecsup.edu.pe>
2026-08-31 13:14:43 -0500

    Proyecto inicial con menu del sistema de multas

---
+++ Semana3/libreria/app/src/main/java/com/lino/libreria/Multas.kt
@@ -0,0 +1,7 @@
+package com.lino.libreria
+
+fun main() {
+    println("=========================================")
+    println(" SISTEMA DE MULTAS - POO")
+    println("=========================================")
+}
