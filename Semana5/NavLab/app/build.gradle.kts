2b4d9e0e9e8229727e520d5b6d1dce6f6c31bd0b
mijaelino21-debug <mijaelino21@gmail.com>
2026-09-22 11:25:04 -0500

    build: agregar dependencia de Navigation Compose 2.7.7

--- Semana5/NavLab/app/build.gradle.kts
+++ Semana5/NavLab/app/build.gradle.kts
@@ -51,5 +51,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
 }
