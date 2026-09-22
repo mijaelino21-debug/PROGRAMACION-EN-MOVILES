8da0a130dde5f73e2bbe22c7fdfed12668954f99
mijaelino21-debug <mijaelino21@gmail.com>
2026-09-22 11:47:12 -0500

    implementar grafo de navegación AppNavigation

--- Semana5/NavLab/app/src/main/java/com/lino/navlab/navigation/AppNavigation.kt
+++ Semana5/NavLab/app/src/main/java/com/lino/navlab/navigation/AppNavigation.kt
@@ -1,2 +1,54 @@
 package com.lino.navlab.navigation
 
+package com.lino.navlab.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.lino.navlab.screens.DetailScreen
+import com.lino.navlab.screens.HomeScreen
+import com.lino.navlab.screens.ListScreen
+import com.lino.navlab.screens.ProfileScreen
+
+@Composable
+fun AppNavigation() {
+
+    val navController = rememberNavController()
+
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+
+
+        composable(Screen.List.route) {
+            ListScreen(navController)
+        }
+
+
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument("itemId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            DetailScreen(navController, itemId)
+        }
+    }
+}
